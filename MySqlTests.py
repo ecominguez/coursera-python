b4916056f631c2dfc3f64b46e4ea2339628372c8
Eduardo Cominguez <ecominguez@gmail.com>
2015-11-16 19:51:47 -0300

    Funciona, toma los datos de la base MySql y lista las universidades.

--- MySqlTests.py
+++ MySqlTests.py
@@ -2,8 +2,13 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='admin',password='admin1',host='192.168.0.185',database='College')
 cursor = cnx.cursor()
-query = ("select cName from College")
+query = ("select cName,state from College")
 cursor.execute(query)
-for (cName) in cursor:
-    print "Universidad {}".format(cName[0])
+
+for (cName,state) in cursor:
+    if state=="CA":
+        print "Universidad {} del la soleada California".format(cName)
+    else:
+        print "Universidad {} del  estado de {}".format(cName,state)
+
 cnx.close()
