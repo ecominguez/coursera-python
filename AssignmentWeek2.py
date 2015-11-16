3ee6549575a3248316c87e1e6085411d1b3721d8
Eduardo Cominguez <ecominguez@gmail.com>
2015-11-16 12:39:16 -0300

    nada

--- AssignmentWeek2.py
+++ AssignmentWeek2.py
@@ -1,2 +1,5 @@
-#just a script to show what the print says...
+# just a script to show what the print says...
 print "Just to show that I have everything installed and running."
+x = 12.1
+print type(x)
+print "Fin!"
