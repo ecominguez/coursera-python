c31ebb78a7a7010f82520ca973f1978d7f9744f5
Eduardo Cominguez <ecominguez@gmail.com>
2015-11-30 12:08:15 -0300

    Agregado del Try/Except. Sale de hacer un Ej del video de la Week 4

--- AssignmentWeek3.4.py
+++ AssignmentWeek3.4.py
@@ -5,9 +5,13 @@
  # and float() to convert the string to a number. Do not worry about error checking
  # the user input - assume the user types numbers properly.
 
-hrs = raw_input("Enter Hours:")
-rph = float(raw_input("Enter rate per hour:"))
-h = float(hrs)
+try:
+    hrs = raw_input("Enter Hours:")
+    rph = float(raw_input("Enter rate per hour:"))
+    h = float(hrs)
+except:
+    print "Error. Enter a numeric value."
+    quit()
 if h>40:
     pay = 40 *rph
     pay += (h-40)* (rph*1.5)
