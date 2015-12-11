ef38705de486d1f293908e3c088cbdc93d54cfbd
Eduardo Cominguez <ecominguez@gmail.com>
2015-12-11 15:34:51 -0300

    Agrego el ejercicio que hice para el quiz de la semana 7. El del libro 5.10

--- Sandbox.py
+++ Sandbox.py
@@ -1,3 +1,55 @@
-hours = raw_input("Enter hours:")
-rate = float(raw_input("Enter rate:"))
-print float(hours) * rate
+# hours = raw_input("Enter hours:")
+# rate = float(raw_input("Enter rate:"))
+# print float(hours) * rate
+
+# Exercise from the video
+# friends = ["John","Glenn","Sally"]
+# for friend in friends :
+#     print "Happy New Year ", friend
+# print "Done!"
+
+
+# looking for the smallest. It is an exercise from the video week7 Largest and Smaller (2nd video)
+# smallest = None
+# print "Before"
+# for value in [9,41,12,3,74,15]:
+#     if smallest is None:
+#         smallest = value
+#     elif smallest > value:
+#         smallest = value
+#     print smallest, value
+#     break
+#     print "Otra vuelta"
+# print "After ",smallest
+
+# Exercise 5.10 of the book the one in the quiz of the assignment.
+largest = None
+smallest = None
+while True:
+    num = raw_input("Enter a number: ")
+    if num == "done" : break
+    try:
+        num = int(num)
+    except:
+        print "Invalid input"
+        continue
+    if smallest is None:
+        largest = num
+        smallest = num
+    elif smallest > num:
+        smallest = num
+    elif largest < num:
+        largest = num
+
+print "Maximum is {}".format(largest)
+print "Minimum is {}".format(smallest)
+
+# And OR. It is an exercise from the video week7 Largest and Smaller (2nd video)
+# smallest = None
+# print "Before"
+# for value in [9,41,12,3,74,15]:
+#     if smallest is None or smallest > value:
+#         smallest = value
+#     print smallest, value
+# print "After ",smallest
+
